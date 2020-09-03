55ca8e2664737704620f4ed290374f1b7ab80f17
takdev <sam@google.com>
2020-09-03 17:47:33 +0100

    chapiter 6

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-import generateUI from "./javascript/generateUI";
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (navigator.onLine) {
-    document.querySelector(".notification").setAttribute("hidden", "");
+function chunkArray(array, size) {
+  if (array.length <= size) {
+    return [array];
   }
+  return [array.slice(0, size), ...chunkArray(array.slice(size), size)];
+}
 
-  window.addEventListener("online", () => {
-    document.querySelector(".notification").setAttribute("hidden", "");
+const dateTimeFormat = Intl.DateTimeFormat("fr");
+
+function generateUI(json){
+  const chunks = chunkArray(json, 3);
+
+  let html = "";
+
+  chunks.forEach(chunk => {
+    html += '<div class="columns">';
+
+    chunk.forEach(repo => {
+      html += `
+            <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">${repo.name}</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+  
+                <div class="content">
+                   ${repo.description}
+                  <br />
+                  Dernière mise à jour: <time datetime="${
+                    repo.updated_at
+                  }">${dateTimeFormat.format(new Date(repo.updated_at))}</time>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    });
+    html += "</div>";
   });
-  window.addEventListener("offline", () => {
-    document.querySelector(".notification").removeAttribute("hidden");
-  });
 
-  let fetchData;
-  if (navigator.onLine) {
-    fetchData = fetch("https://api.github.com/users/EmmanuelDemey/repos")
-      .then((response) => response.json())
-      .then((data) => localforage.setItem("data", data));
-  } else {
-    fetchData = localforage.getItem("data");
-  }
+  document.querySelector(".container").innerHTML = html;
+}
 
-  fetchData.then((json) => generateUI(json));
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("https://api.github.com/users/EmmanuelDemey/repos")
+	.then(response => response.json())
+    .then(json => generateUI(json));
+
 });
--- sw.js
+++ sw.js
@@ -1,38 +1,3 @@
-importScripts(
-  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"
-);
-
-const cacheName = "blog-v1";
-const { assets } = global.serviceWorkerOption;
-
-const files = [
-  "/",
-  ...assets,
-  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js",
-  "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css",
-  "https://bulma.io/images/placeholders/1280x960.png",
-  "https://bulma.io/images/placeholders/96x96.png",
-];
-
-self.addEventListener("install", (e) => {
-  caches.open(cacheName).then((cache) => {
-    cache.addAll(files);
-  });
-});
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(
-        keyList.map(function (key) {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
 self.addEventListener("fetch", (e) => {});
 
 self.addEventListener("fetch", (event) => {
@@ -62,42 +27,13 @@ self.addEventListener("fetch", (event) => {
 
         return response.json().then((json) => {
           const formattedResponse = json.map((j) => ({
-            id: j.id,
             name: j.name,
             description: j.description || "",
             updated_at: j.updated_at,
             avatar_url: j.owner.avatar_url,
-            html_url: j.html_url,
           }));
 
           return new Response(JSON.stringify(formattedResponse));
-        });
-      })
-    );
-  } else {
-    event.respondWith(
-      caches
-        .open(cacheName)
-        .then((cache) => cache.match(event.request))
-        .then((response) => response || fetch(event.request))
-    );
-  }
-});
-
-self.addEventListener("sync", (event) => {
-  if (event.tag === "syncFavorites") {
-    console.log("Synchronisation en cours");
-
-    if (Notification.permission === "granted") {
-      registration.showNotification("Synchronisation en cours");
-    }
-
-    event.waitUntil(
-      localforage.getItem("favorites").then((favorites) => {
-        return fetch("http://localhost:3000/favorites", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(favorites),
         });
       })
     );
